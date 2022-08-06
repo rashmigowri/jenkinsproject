438ebb1628f08a66a6624d7645464309d1f18f27
user <user@DESKTOP-BDO1GJM>
2022-08-06 21:43:40 +0530

    jenkins

---
+++ src/test/java/scripts/test3.java
@@ -0,0 +1,16 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test3 {
+	
+
+	@Test 
+	public void tc3() {
+	Reporter.log("hello jenkins",true);	
+		
+		
+	}
+
+}
